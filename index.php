8a52503c57efe4e9bd04e33066446f3fe0f6023c
Gerardo Zelaya <gera121134@gmail.com>
2021-03-18 23:40:42 -0600

    Update1

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Club de Ajedrez - Universidad de Sonsonate</title>
+    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hamburgers/1.1.3/hamburgers.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="barra-superior">
+        <div class="contenedor">
+            <span>Unidad de deportes USO | <a href="contacto.php" class="mensaje">Envíanos un mensaje!</a></span>
+        </div>
+    </div>
+    <header class="header">
+        <div class="contenedor">
+            <div class="barra blanco">
+                <div class="logo">
+                    <a href="/">
+                    <img src="img/logo_uso.png" alt="Logo Universidad de Sonsonate">
+                    </a>
+                    <div class="contenedor-texto">
+                        <h2>Universidad de Sonsonate</h2>
+                        <p>SCIENTIAE ET BONIS ARTIBUS</p>
+                    </div>
+                </div>
+                <button class="hamburger btn-menu hamburger--vortex" type="button">
+                    <span class="hamburger-box">
+                      <span class="hamburger-inner"></span>
+                    </span>
+                  </button>                  
+                <nav class="navegacion">
+                    <ul class="mostrar_menu">
+                        <li><a href="atletismo.php">Atletismo</a></li>
+                        <li><a href="futbol.php">Fútbol</a></li>
+                        <li><a href="baloncesto.php">Baloncesto</a></li>
+                        <li><a href="juegos-de-mesa.php">Juegos de mesa</a></li>
+                        <li><a href="defensa.php">Defensa</a></li>
+                        <li><a href="gimnasio.php">Gimnasio</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="inicio portada blanco">
+                <div class="overlay">
+                    <h1>
+                        Club deportivo de Ajedrez <span>Universidad de Sonsonate</span>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main class="main  blanco  contenedor">
+        <section class="seccion">
+            <h2>¿Quiénes somos?</h2>
+            <article>
+                <div class="card">
+                    <div class="card-imagen">
+                        <img src="img/acercade.png" alt="acerca de">
+                    </div>
+                    <div class="card-info">
+                        <h3>Acerca de...</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora ullam officia corporis eius vel hic eos, omnis tempore quaerat, possimus accusantium voluptates. Nam repellendus eligendi eius officia voluptate asperiores inventore.</p>
+
+                        <a href="#" class="btn btn-verde">Leer más</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-imagen">
+                        <img src="img/historia.png" alt="acerca de">
+                    </div>
+                    <div class="card-info">
+                        <h3>Nuestra historia</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora ullam officia corporis eius vel hic eos, omnis tempore quaerat, possimus accusantium voluptates. Nam repellendus eligendi eius officia voluptate asperiores inventore.</p>
+
+                        <a href="#" class="btn btn-verde">Leer más</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-imagen">
+                        <img src="img/logros.png" alt="acerca de">
+                    </div>
+                    <div class="card-info">
+                        <h3>Nuestros logros</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora ullam officia corporis eius vel hic eos, omnis tempore quaerat, possimus accusantium voluptates. Nam repellendus eligendi eius officia voluptate asperiores inventore.</p>
+
+                        <a href="#" class="btn btn-verde">Leer más</a>
+                    </div>
+                </div>
+            </article>
+        </section>
+        <section>
+            <h2>Nuestra estrategia</h2>
+            <div class="grid-dos">
+                <div class="imagen">
+                    <img src="https://androidayuda.com/app/uploads-androidayuda.com/2020/06/apertura-juegos-ajedrez.jpg" alt="imagen">
+                </div>
+                <div class="texto-estrategia">
+                    <h3>Principios de juego:</h3>
+                    <p>Estas son las estrategias que como club de ajedrez de la <span class="texto-verde">Universidad de Sonsonate</span> utilizamos para asegurar nuestras victorias en los diferentes torneos:</p>
+                    <ul>
+                        <li>Controlar el centro.</li>
+                        <li>Conectar las dos torres.</li>
+                        <li>Realizar el enroque lo más pronto posible.</li>
+                        <li>Practicar mejor las aperturas.</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="grid-dos">
+                <div class="texto-estrategia">
+                    <h3>Nuestras Aperturas:</h3>
+                    <p>Estas son las estrategias que como club de ajedrez de la <span class="texto-verde">Universidad de Sonsonate</span> utilizamos para asegurar nuestras victorias en los diferentes torneos:</p>
+                    <ul>
+                        <li>Apertura "Gambito de Dama".</li>
+                        <li>Apertura Inglesa</li>
+                        <li>Defensa Karo-can</li>
+                        <li>Defensa india del rey</li>
+                    </ul>
+                </div>
+                <div class="imagen">
+                    <img src="https://i.pinimg.com/originals/93/0a/82/930a82e6d8c52c8a84a7d2cef2746a9f.jpg" alt="imagen">
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <p>
+            Universidad de Sonsonate &copy;
+        </p>
+    </footer>
+    <script src="js/js.js"></script>
+</body>
+</html>
